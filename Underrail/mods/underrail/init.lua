db63faa030c323afe27d3ce5dc1e91224bd5c15f
himerian <geazeilort@yandex.ru>
2022-01-09 11:23:43 +0400

    Metal Crates
    Adds metal crates at Underrail game

--- Underrail/mods/underrail/init.lua
+++ Underrail/mods/underrail/init.lua
@@ -13,23 +13,38 @@ minetest.register_node("underrail:techbrick", {
 	groups = {cracky = 3, stone = 2},
 	})
 	
-	minetest.register_node("underrail:concrete_1", {
+minetest.register_node("underrail:concrete_1", {
 	description = "SGS concrete floor 1",
 	tiles = {"concrete_1.png"},
 	is_ground_content = false,
 	groups = {cracky = 3, stone = 2},
 	})
 	
-	minetest.register_node("underrail:wall_1", {
+minetest.register_node("underrail:wall_1", {
 	description = "SGS wall",
 	tiles = {"wall_1.png"},
 	is_ground_content = false,
 	groups = {cracky = 3, stone = 2},
 	})
 	
-	minetest.register_node("underrail:underrail_wall_1", {
+minetest.register_node("underrail:underrail_wall_1", {
 	description = "Basic Underrail wall",
 	tiles = {"underrail_wall_1.png"},
 	is_ground_content = false,
 	groups = {cracky = 3, stone = 2},
 	})
+
+minetest.register_node("underrail:ur_metal_crate", {
+    description = "Metal Crate",
+    tiles = {
+        "ur_metal_crate_up.png", -- up
+        "ur_metal_crate_down.png", -- down
+        "ur_metal_crate_right.png", -- right
+        "ur_metal_crate_left.png", -- left
+        "ur_metal_crate_back.png", -- back
+        "ur_metal_crate_front.png" -- front
+    },
+    is_ground_content = false,
+    groups = {cracky = 3, stone = 2},
+    paramtype2 = "facedir", -- faced to the player when set
+})
