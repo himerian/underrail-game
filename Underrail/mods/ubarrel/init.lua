45d96511feae71fdc8703c62054c0a8f512da3e7
himerian <geazeilort@yandex.ru>
2022-01-22 21:26:30 +0400

    Crate, mushroom & locker
    Added metal crates (no inventory), blue mushrooms, and floor lockers.

--- Underrail/mods/ubarrel/init.lua
+++ Underrail/mods/ubarrel/init.lua
@@ -2,8 +2,8 @@
 
 ubarrel = {}
 
-minetest.register_node("ubarrel:ubarrel", {
-	description = "ubarrel",
+minetest.register_node("ubarrel:zoner_barrel", {
+	description = "Zoner Barrel",
 	drawtype = "mesh",
 	mesh = "barrel.obj",
 	tiles = {"ubarrel_barrel.png"},
@@ -16,4 +16,18 @@ minetest.register_node("ubarrel:ubarrel", {
 		--fixed = {-0.3, -0.5, -0.3, 0.3, 0, 0.3}
 	},
 	--inventory_image = "3dmushrooms_brown_inv.png"
+})
+
+minetest.register_node("ubarrel:test_barrel", {
+	description = "Test Barrel",
+	drawtype = "mesh",
+	mesh = "test_barrel.obj",
+	tiles = {"test_barrel.png"},
+	paramtype = "light",
+	is_ground_content = false,
+	groups = {cracky = 3, stone = 2},
+		selection_box = {
+		type = "regular",
+		--fixed = {-0.3, -0.5, -0.3, 0.3, 0, 0.3}
+	},
 })
